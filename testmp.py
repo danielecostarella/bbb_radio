b4f3773bca8c03c35e43c2388bfc8ffaa27ad4b1
Mario Pucciarelli <pucciarelli.mario@gmail.com>
2014-02-22 10:16:49 +0000

    Modificata gestione dei tasti per evitare problemi con aggiornamento del s.o. e lettura degli eventi

--- testmp.py
+++ testmp.py
@@ -5,12 +5,20 @@ import sys
 import re
 import lcd
 import threading
+import Adafruit_BBIO.GPIO as GPIO
 
 stations={'JAZZ': 'http://www.radioswissjazz.ch/live/aacp.m3u', 'Virgin Radio': 'http://shoutcast.unitedradio.it:1301','Deejay': 'http://mp3.kataweb.it:8000/RadioDeejay','105Hits': 'http://shoutcast.unitedradio.it:1109/listen.pls'}
 volume = 25 	#default volume value for mplayer 
 streamTitle=""
 pause = 0
 lines = ["", "", "", ""]
+
+# Pin setup
+GPIO.setup("P9_21", GPIO.IN)
+GPIO.setup("P9_22", GPIO.IN)
+GPIO.setup("P9_23", GPIO.IN)
+GPIO.setup("P9_24", GPIO.IN)
+
 
 #p = subprocess.Popen(["mplayer","-ao","alsa:device=hw=1.0", "-quiet", "-slave", "http://shoutcast.unitedradio.it:1301"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 #p = subprocess.Popen(["mplayer","-ao","alsa:device=hw=1.0", "-slave", "-playlist", "http://www.radioswissjazz.ch/live/aacp.m3u"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -122,21 +130,119 @@ def updateLCD():
         lcd.writeln(4, lines[3])
         #lcd.writeln(2, streamTitle[0:20])
         #time.sleep(1)
-        
+
+
+def inputButtons():
+    old_switch_state1 = 1
+    old_switch_state2 = 1
+    old_switch_state3 = 1
+    old_switch_state4 = 1
+    print "***** Benvenuto nel Thread BUTTON ******"
+    while(1):
+        new_switch_state1 = GPIO.input("P9_21")
+        if new_switch_state1 == 0 and old_switch_state1 == 1 :
+            print('Do not press this button 1 again!')
+            function1()
+            time.sleep(0.1)
+        old_switch_state1 = new_switch_state1
+        new_switch_state2 = GPIO.input("P9_22")
+        if new_switch_state2 == 0 and old_switch_state2 == 1 :
+            print('Do not press this button 2 again!')
+            function2()
+            time.sleep(0.1)
+        old_switch_state2 = new_switch_state2
+        new_switch_state3 = GPIO.input("P9_23")
+        if new_switch_state3 == 0 and old_switch_state3 == 1 :
+            print('Do not press this button 3 again!')
+            function3()
+            time.sleep(0.1)
+        old_switch_state3 = new_switch_state3
+        new_switch_state4 = GPIO.input("P9_24")
+        if new_switch_state4 == 0 and old_switch_state4 == 1 :
+            print('Do not press this button 4 again!')
+            function4()
+            time.sleep(0.1)
+        old_switch_state4 = new_switch_state4
+        #if not GPIO.input("P9_21"): function1()
+        #if not GPIO.input("P9_22"): function2()
+        #if not GPIO.input("P9_23"): function3()
+        #if not GPIO.input("P9_24"): function4()
+
+
+def inputButton1():
+    old_switch_state = 1
+    print "***** Benvenuto nel Thread BUTTON ******"
+    while(1):
+        new_switch_state = GPIO.input("P9_21")
+        if new_switch_state == 0 and old_switch_state == 1 :
+            print('Do not press this button 1 again!')
+            time.sleep(0.1)
+        old_switch_state = new_switch_state
+        #if not GPIO.input("P9_21"): function1()
+        #if not GPIO.input("P9_22"): function2()
+        #if not GPIO.input("P9_23"): function3()
+        #if not GPIO.input("P9_24"): function4()
+
+def inputButton2():
+    old_switch_state = 1
+    print "***** Benvenuto nel Thread BUTTON ******"
+    while(1):
+        new_switch_state = GPIO.input("P9_22")
+        if new_switch_state == 0 and old_switch_state == 1 :
+            print('Do not press this button 2 again!')
+            time.sleep(0.1)
+        old_switch_state = new_switch_state
+
+def inputButton3():
+    old_switch_state = 1
+    print "***** Benvenuto nel Thread BUTTON ******"
+    while(1):
+        new_switch_state = GPIO.input("P9_23")
+        if new_switch_state == 0 and old_switch_state == 1 :
+            print('Do not press this button 3 again!')
+            time.sleep(0.1)
+        old_switch_state = new_switch_state
+
+def inputButton4():
+    old_switch_state = 1
+    print "***** Benvenuto nel Thread BUTTON ******"
+    while(1):
+        new_switch_state = GPIO.input("P9_24")
+        if new_switch_state == 0 and old_switch_state == 1 :
+            print('Do not press this button 4 again!')
+            time.sleep(0.1)
+        old_switch_state = new_switch_state
 
 t2 = threading.Thread(target=updateLCD)
 t2.start()
 
+#inputBtn1 = threading.Thread(target=inputButton1)
+#inputBtn1.start()
+
+#inputBtn2 = threading.Thread(target=inputButton2)
+#inputBtn2.start()
+
+#inputBtn3 = threading.Thread(target=inputButton3)
+#inputBtn3.start()
+
+#inputBtn4 = threading.Thread(target=inputButton4)
+#inputBtn4.start()
+
+inputBtns = threading.Thread(target=inputButtons)
+inputBtns.start()
 
 # read event streaming
-evt_file = open("/dev/input/event1", "rb")
-while(1):
+#evt_file = open("/dev/input/event1", "rb")
+while(1): time.sleep(1)
+
     #lcd.writeln(2, streamTitle[0:20])
-    evt = evt_file.read(16) # Read the event
-    evt_file.read(16)       # Discard the debounce event 
-    code = ord(evt[10])
+    #evt = evt_file.read(16) # Read the event
+    #evt_file.read(16)       # Discard the debounce event 
+    #code = ord(evt[10])
     #print "Button "+str(code)+" pressed with direction "+str(ord(evt[12]))
-    if str(code)=='1' and str(ord(evt[12]))=='1': function1()
-    elif str(code)=='2' and str(ord(evt[12]))=='1': function2()
-    elif str(code)=='3' and str(ord(evt[12]))=='1': function3()
-    elif str(code)=='4' and str(ord(evt[12]))=='1': function4()
+    
+
+    #if str(code)=='1' and str(ord(evt[12]))=='1': function1()
+    #elif str(code)=='2' and str(ord(evt[12]))=='1': function2()
+    #elif str(code)=='3' and str(ord(evt[12]))=='1': function3()
+    #elif str(code)=='4' and str(ord(evt[12]))=='1': function4()
